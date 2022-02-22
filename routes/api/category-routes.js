73fe68ec06f7680741ef974e7f40c20517d899d3
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2022-02-22 23:01:34 +0000

    add update category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,12 +42,33 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  //create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+
+    res.status(200).json(newCategory);
+  }
+  catch (err) {
+    res.status(400).json (err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-});
+  try {
+    const updateCategory = await Category.update (req.body,{
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!updateCategory) {
+      res.status(404).json({ message: "No updates with that id"});
+    }
+    res.status(200).json(updateCategory);
+    } catch (err) {
+    res.status(500).json(err);
+  }
+  });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
