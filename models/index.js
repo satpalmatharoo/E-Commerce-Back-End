abc864ecad8a3b3944fcfd26aec860ed6893f329
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2022-02-28 23:32:10 +0000

    debug

--- models/index.js
+++ models/index.js
@@ -7,34 +7,26 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey:"category_id",
-  onDelete: "CASADE",
+  onDelete: "CASCADE",
 }),
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: "category_id",
-  onDelete: "CASCADE"
+
 })
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique:false
-  },
-  as: "product_tags"
+  through: ProductTag,
+  foreignKey: "product_id"
 });
 
 // Tags belongToMany Products (through ProductTag)
-Product.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique:false
-  },
-  as: "tagged_products"
-});
-
-
+Tag.belongsToMany(Product,{
+  through: ProductTag,
+  foreignKey: "tag_id"
+})
 module.exports = {
   Product,
   Category,
